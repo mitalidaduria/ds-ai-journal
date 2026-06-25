ca219ef70823b3f8211741cff46994105368279a
Mitali Daduria <mitalidaduriaj@gmail.com>
2026-06-25 17:02:29 +0530

    Highlight importance of median in transaction analysis
    Added a comment emphasizing the importance of the median in analyzing transaction values.

--- Stats_numpy.py
+++ Stats_numpy.py
@@ -7,7 +7,7 @@ import numpy as np
 txn_amounts = np.array([150, 299, 450, 1200, 899, 75, 4500, 250, 180, 620, 980, 3200, 90, 750, 1100, 550, 8900, 420])
 
 # Central Tendency
-
+###( When analyzing business metrics like user transaction values, platform latency, or system processing times, always check the median. The mean can easily lie to you if a few massive outliers skew the data.)
 print("Mean:", np.mean(txn_amounts).round(2))  #average
 print("Median:", np.median(txn_amounts))       # middle value
 
